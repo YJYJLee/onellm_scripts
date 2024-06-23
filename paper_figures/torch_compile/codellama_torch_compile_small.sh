0304f99e223a989bd71a03e7109f41c338dd9556
YJYJLee <jane001356@gmail.com>
2024-06-23 08:40:59 +0000

    fix

--- paper_figures/torch_compile/codellama_torch_compile_small.sh
+++ paper_figures/torch_compile/codellama_torch_compile_small.sh
@@ -1,3 +1,3 @@
 BATCH_SIZE=$1
-TORCH_COMPILE=True BATCH_SIZE=$BATCH_SIZE accelerate launch /fsx-atom/yejinlee/2024_paper_projects/bigcode-evaluation-harness/main.py --model meta-llama/CodeLlama-7b-hf --max_length_generation 10000 --tasks humaneval --temperature 0.2 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
-TORCH_COMPILE=True BATCH_SIZE=$BATCH_SIZE accelerate launch /fsx-atom/yejinlee/2024_paper_projects/bigcode-evaluation-harness/main.py --model meta-llama/CodeLlama-7b-hf --max_length_generation 10000 --tasks mbpp --temperature 0.1 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
+TORCH_COMPILE=True BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta-llama/CodeLlama-7b-hf --max_length_generation 10000 --tasks humaneval --temperature 0.2 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
+TORCH_COMPILE=True BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta-llama/CodeLlama-7b-hf --max_length_generation 10000 --tasks mbpp --temperature 0.1 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
