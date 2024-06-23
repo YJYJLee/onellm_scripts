9c67aae6cd1c2a61d54557319be329b8ae2ed3e5
YJYJLee <jane001356@gmail.com>
2024-06-23 07:59:21 +0000

    fix

--- paper_figures/torch_compile/chameleon_torch_compile.sh
+++ paper_figures/torch_compile/chameleon_torch_compile.sh
@@ -1,5 +1,5 @@
 # Img2Txt, ImgTxt2Txt
-TORCH_COMPILE=True FSD=/fsx-onellm/mingdachen/onellm-eval-data-tok python stool.py run cm3v21_30b_test onellm_eval/run.py --sweep configs/sweeps/cm3_v2_image.yaml --mem 480 --ncpu 10 --ngpu 1 --ntasks 1 --nodes 1 --account atom --anaconda /fsx-atom/yejinlee/condaenv/onellm3
+TORCH_COMPILE=True FSD=/fsx-onellm/mingdachen/onellm-eval-data-tok python stool.py run compile_test onellm_eval/run.py --sweep configs/sweeps/cm3_v2_image.yaml --mem 480 --ncpu 10 --ngpu 1 --ntasks 1 --nodes 1 --account atom --anaconda /fsx-atom/yejinlee/condaenv/onellm3
 
 # # Txt2Img
-# TORCH_COMPILE=True FSD=/fsx-onellm/mingdachen/onellm-eval-data-tok python stool.py run cm3v21_30b_test scripts/cm3v2/gen_images.py --sweep configs/sweeps/cm3_v2_image_gen.yaml --mem 480 --ncpu 10 --ngpu 1 --ntasks 1 --nodes 1 --account atom --anaconda /fsx-checkpoints/yejinlee/condaenv/onellm3 --image_generation
+# TORCH_COMPILE=True FSD=/fsx-onellm/mingdachen/onellm-eval-data-tok python stool.py run compile_test scripts/cm3v2/gen_images.py --sweep configs/sweeps/cm3_v2_image_gen.yaml --mem 480 --ncpu 10 --ngpu 1 --ntasks 1 --nodes 1 --account atom --anaconda /fsx-checkpoints/yejinlee/condaenv/onellm3 --image_generation
