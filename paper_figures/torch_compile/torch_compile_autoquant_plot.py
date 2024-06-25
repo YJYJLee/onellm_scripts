7e8e107da540aa12dcb356febba2f6fe11fd8d44
YJYJLee <jane001356@gmail.com>
2024-06-25 10:12:35 +0000

    torch graph fix

--- paper_figures/torch_compile/torch_compile_autoquant_plot.py
+++ paper_figures/torch_compile/torch_compile_autoquant_plot.py
@@ -31,144 +31,155 @@ def get_latency(file_path):
     return timer_result
 
 def get_folder(dataset, bs, exp_name):
-    if "torch_compile" in exp_name:
+    if "torch_compile" == exp_name:
         chameleon_prefix = "compile_test"
+    elif "torch_compile_baseline" in exp_name:
+        chameleon_prefix = "compile_test_baseline"
+    elif "torch_compile_autoquant" in exp_name:
+        chameleon_prefix = "quant_compile"
     else:
-        chameleon_prefix = "cm3v21_30b_test"
+        if "7B" in dataset:
+            chameleon_prefix = "cm3v21_109m_sft_test"
+        else:
+            chameleon_prefix = "cm3v21_30b_test"
 
-    if dataset == "MSCOCO":
+    if dataset == "MSCOCO-34B":
         return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.coco."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
-    elif dataset == "Flickr30k":
+    elif dataset == "Flickr30k-34B":
         return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.flickr30k."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
-    elif dataset == "TextVQA":
+    elif dataset == "TextVQA-34B":
         return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_txt_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.textvqa."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
-    elif dataset == "OKVQA":
+    elif dataset == "OKVQA-34B":
         return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_txt_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.okvqa."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
-    elif dataset == "Vizwiz":
+    elif dataset == "Vizwiz-34B":
         return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_txt_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.vizwiz."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
-    elif dataset == "Hellaswag":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/txt_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.hellaswag."+str(ns)+"_shot.mbs."+str(bs)+".umca.True.gm.text/"
-    elif dataset == "Arc_easy":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/txt_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.arc_easy."+str(ns)+"_shot.mbs."+str(bs)+".umca.True.gm.text/"
-    elif dataset == "HumanEval":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/HumanEval_codellama/meta-llama/CodeLlama-34b-hf/batch_size_"+str(bs)
-    elif dataset == "MBPP":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/MBPP_codellama/meta-llama/CodeLlama-34b-hf/batch_size_"+str(bs)
-    elif dataset == "HumanEval_small":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/HumanEval_codellama/meta-llama/CodeLlama-7b-hf/batch_size_"+str(bs)
-    elif dataset == "MBPP_small":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/MBPP_codellama/meta-llama/CodeLlama-7b-hf/batch_size_"+str(bs)
-    elif dataset == "Coco_Image":
+    if dataset == "MSCOCO-7B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_to_txt/"+chameleon_prefix+".mn.cm3v21_109m_sft.t.coco."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
+    elif dataset == "Flickr30k-7B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_to_txt/"+chameleon_prefix+".mn.cm3v21_109m_sft.t.flickr30k."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
+    elif dataset == "TextVQA-7B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_txt_to_txt/"+chameleon_prefix+".mn.cm3v21_109m_sft.t.textvqa."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
+    elif dataset == "OKVQA-7B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_txt_to_txt/"+chameleon_prefix+".mn.cm3v21_109m_sft.t.okvqa."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
+    elif dataset == "Vizwiz-7B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/img_txt_to_txt/"+chameleon_prefix+".mn.cm3v21_109m_sft.t.vizwiz."+str(ns)+"_shot.cm3v2_template.mbs."+str(bs)+".umca.True.gm.text.ev.False/"
+    elif dataset == "Coco_Image-34B":
         return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/txt_to_img/"+chameleon_prefix+".mn.cm3v21_30b_test.t.coco_image."+str(ns)+"_shot.bs.500.c.6.t.1.0.t.0.9.s.1.ncs."+str(bs)+".en.image_gen.g.True/%j/image_gen/mn.cm3v21_30b_test.t.coco_image."+str(ns)+"_shot.usecfg.True.cfg.6.temp.1.0.topp.0.9.seed.1/"
-    elif dataset == "Coco_Image_small":
+    elif dataset == "Coco_Image-7B":
         return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/txt_to_img/"+chameleon_prefix+".mn.cm3v21_109m_sft.t.coco_image."+str(ns)+"_shot.bs.500.c.6.t.1.0.t.0.9.s.1.ncs."+str(bs)+".en.image_gen.g.True/%j/image_gen/mn.cm3v21_109m_sft.t.coco_image."+str(ns)+"_shot.usecfg.True.cfg.6.temp.1.0.topp.0.9.seed.1/"
-    elif dataset == "Partiprompts":
+    elif dataset == "Partiprompts-34B":
         return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/txt_to_img/"+chameleon_prefix+".mn.cm3v21_30b_test.t.partiprompts."+str(ns)+"_shot.bs.500.c.6.t.1.0.t.0.9.s.1.ncs."+str(bs)+".en.image_gen.g.True/%j/image_gen/mn.cm3v21_30b_test.t.partiprompts."+str(ns)+"_shot.usecfg.True.cfg.6.temp.1.0.topp.0.9.seed.1/"
-    elif dataset == "S2ST":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/"+("batched" if exp_name=="latency_distribution_w_warmup" else "")+"/S2ST/batch_size_"+str(bs)+"/"
-    elif dataset == "S2TT":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/"+("batched" if exp_name=="latency_distribution_w_warmup" else "")+"/S2TT/batch_size_"+str(bs)+"/"
-    elif dataset == "T2TT":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/"+("batched" if exp_name=="latency_distribution_w_warmup" else "")+"/T2TT/batch_size_"+str(bs)+"/"
-    elif dataset == "T2ST":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/"+("batched" if exp_name=="latency_distribution_w_warmup" else "")+"/T2ST/batch_size_"+str(bs)+"/"
-    elif dataset == "HSTU-1M":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/HSTU/num_embeddings_1000000_batch_size_"+str(bs)+"/"
-    elif dataset == "HSTU-15M":
-        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/HSTU/num_embeddings_15000000_batch_size_"+str(bs)+"/"
+    elif dataset == "Partiprompts-7B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/txt_to_img/"+chameleon_prefix+".mn.cm3v21_109m_sft.t.partiprompts."+str(ns)+"_shot.bs.500.c.6.t.1.0.t.0.9.s.1.ncs."+str(bs)+".en.image_gen.g.True/%j/image_gen/mn.cm3v21_109m_sft.t.partiprompts."+str(ns)+"_shot.usecfg.True.cfg.6.temp.1.0.topp.0.9.seed.1/"
+    # elif dataset == "Hellaswag":
+    #     return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/txt_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.hellaswag."+str(ns)+"_shot.mbs."+str(bs)+".umca.True.gm.text/"
+    # elif dataset == "Arc_easy":
+    #     return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/txt_to_txt/"+chameleon_prefix+".mn.cm3v21_30b_test.t.arc_easy."+str(ns)+"_shot.mbs."+str(bs)+".umca.True.gm.text/"
+    elif dataset == "HumanEval-34B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/HumanEval_codellama/meta-llama/CodeLlama-34b-hf/batch_size_"+str(bs)
+    elif dataset == "HumanEval-7B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/HumanEval_codellama/meta-llama/CodeLlama-7b-hf/batch_size_"+str(bs)
+    elif dataset == "MBPP-34B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/MBPP_codellama/meta-llama/CodeLlama-34b-hf/batch_size_"+str(bs)
+    elif dataset == "MBPP-7B":
+        return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/MBPP_codellama/meta-llama/CodeLlama-7b-hf/batch_size_"+str(bs)
+    # elif dataset == "S2ST":
+    #     return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/"+("batched" if exp_name=="latency_distribution_w_warmup" else "")+"/S2ST/batch_size_"+str(bs)+"/"
+    # elif dataset == "S2TT":
+    #     return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/"+("batched" if exp_name=="latency_distribution_w_warmup" else "")+"/S2TT/batch_size_"+str(bs)+"/"
+    # elif dataset == "T2TT":
+    #     return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/"+("batched" if exp_name=="latency_distribution_w_warmup" else "")+"/T2TT/batch_size_"+str(bs)+"/"
+    # elif dataset == "T2ST":
+    #     return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/"+("batched" if exp_name=="latency_distribution_w_warmup" else "")+"/T2ST/batch_size_"+str(bs)+"/"
+    # elif dataset == "HSTU-1M":
+    #     return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/HSTU/num_embeddings_1000000_batch_size_"+str(bs)+"/"
+    # elif dataset == "HSTU-15M":
+    #     return "/fsx-atom/yejinlee/paper_submission_results/"+exp_name+"/"+str(n_gpu)+"gpu_1node/HSTU/num_embeddings_15000000_batch_size_"+str(bs)+"/"
     else:
+        print(dataset)
         assert False
 
+
+def plot(batch_size_dict):
+    baseline = []
+
+    for k, bs in batch_size_dict.items():
+        baseline.append(sum([np.average(v[5:]) for v in get_latency(get_folder(k, bs, "latency_distribution_w_warmup")).values()]))
+
+    # torch_compile_baseline = []
+    # for k, bs in batch_size_dict.items():
+    #     torch_compile_baseline.append(sum([np.average(v[5:]) for v in get_latency(get_folder(k, bs, "torch_compile_baseline")).values()]))
+
+    # torch_compile_baseline = [a/b if b>0 else (2 if a>0 else 0) for a, b in zip(torch_compile_baseline, baseline)]
+
+    torch_compile = []
+    for k, bs in batch_size_dict.items():
+        torch_compile.append(sum([np.average(v[5:]) for v in get_latency(get_folder(k, bs, "torch_compile")).values()]))
+
+    torch_compile = [a/b if b>0 else (2 if a>0 else 0) for a, b in zip(torch_compile, baseline)]
+
+    torch_compile_autoquant = []
+    for k, bs in batch_size_dict.items():
+        torch_compile_autoquant.append(sum([np.average(v[5:]) for v in get_latency(get_folder(k, bs, "torch_compile_autoquant")).values()]))
+    torch_compile_autoquant = [a/b if b>0 else (2 if a>0 else 0) for a, b in zip(torch_compile_autoquant, baseline)]
+
+    baseline = [b/b if b>0 else 0 for b in baseline]
+
+    # create data 
+    x = np.arange(len(baseline)) 
+    width = 0.2
+
+    fig, ax = plt.subplots(1, figsize=(10, 6), layout='tight')
+
+    # plot data in grouped manner of bar type 
+    # plt.bar(x-0.3, baseline, width, color='cyan') 
+    # plt.bar(x-0.1, torch_compile_baseline, width, color='magenta') 
+    # plt.bar(x+0.1, torch_compile, width, color='orange') 
+    # plt.bar(x+0.3, torch_compile_autoquant, width, color='green') 
+
+    plt.bar(x-0.2, baseline, width, color='cyan') 
+    plt.bar(x, torch_compile, width, color='orange') 
+    plt.bar(x+0.2, torch_compile_autoquant, width, color='green') 
+
+    plt.xticks(x, list(batch_size_dict.keys()), rotation=90) 
+    plt.xlabel("Workloads") 
+    plt.ylabel("Normalized Speedup") 
+    # plt.legend(["Baseline", "Torch.compile Baseline", "Torch.compile", "Torch.compile+Autoquant"],ncol=4)
+    plt.legend(["Baseline", "Torch.compile", "Torch.compile+Autoquant"],ncol=3)
+    plt.grid(lw=0.2)
+    plt.savefig("/fsx-atom/yejinlee/analysis_figures/torch_compile/batch_size_"+str(batch_size_dict["MSCOCO-34B"])+".pdf", bbox_inches = 'tight')
+    plt.show() 
+
+
 batch_size_dict = {
-    "MSCOCO": 1,
-    "Vizwiz": 1,
-    "Coco_Image": 1,
-    "Coco_Image_small": 1,
-    "HumanEval": 1,
-    "HumanEval_small": 1,
-    "S2ST": 1,
-    "S2TT": 1,
-    "T2ST": 1,
-    "T2TT": 1,
+    "MSCOCO-34B": 1,
+    "MSCOCO-7B": 1,
+    "Flickr30k-34B": 1,
+    "Flickr30k-7B": 1,
+    "TextVQA-34B": 1,
+    "TextVQA-7B": 1,
+    "OKVQA-34B": 1,
+    "OKVQA-7B": 1,
+    "Vizwiz-34B": 1,
+    "Vizwiz-7B": 1,
+    "Coco_Image-34B": 1,
+    "Coco_Image-7B": 1,
+    "Partiprompts-34B": 1,
+    "Partiprompts-7B": 1,
+    "HumanEval-34B": 1,
+    "HumanEval-7B": 1,
+    "MBPP-34B": 1,
+    "MBPP-7B": 1,
+    # "S2ST": 1,
+    # "S2TT": 1,
+    # "T2ST": 1,
+    # "T2TT": 1,
 }
+batch_sizes = [1,4,8,16,32,64,128]
+# batch_sizes = [1]
+for bs in batch_sizes:
+    for k, v in batch_size_dict.items():
+        batch_size_dict[k] = bs
 
-baseline = []
-
-for k, bs in batch_size_dict.items():
-    baseline.append(sum([np.average(v[5:]) for v in get_latency(get_folder(k, bs, "latency_distribution_w_warmup")).values()]))
-
-torch_compile = []
-for k, bs in batch_size_dict.items():
-    baseline.append(sum([np.average(v[5:]) for v in get_latency(get_folder(k, bs, "torch_compile")).values()]))
-
-torch_compile_autoquant = []
-for k, bs in batch_size_dict.items():
-    baseline.append(sum([np.average(v[5:]) for v in get_latency(get_folder(k, bs, "torch_compile_autoquant")).values()]))
-
-# baseline = [
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("MSCOCO", batch_size_dict["MSCOCO"], "latency_distribution_w_warmup")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Vizwiz", batch_size_dict["Vizwiz"], "latency_distribution_w_warmup")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Coco_Image", batch_size_dict["Coco_Image"], "latency_distribution_w_warmup")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Coco_Image_small", batch_size_dict["Coco_Image_small"], "latency_distribution_w_warmup")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("HumanEval", batch_size_dict["HumanEval"], "latency_distribution_w_warmup")).values()]),
-#     # sum([np.average(v[5:]) for v in get_latency(get_folder("HumanEval_small", batch_size_dict["HumanEval_small"], "latency_distribution_w_warmup")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("S2ST", batch_size_dict["S2ST"], "latency_distribution_w_warmup")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("S2TT", batch_size_dict["S2TT"], "latency_distribution_w_warmup")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("T2ST", batch_size_dict["T2ST"], "latency_distribution_w_warmup")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("T2TT", batch_size_dict["T2TT"], "latency_distribution_w_warmup")).values()]),
-# ]
-
-
-# torch_compile = [
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("MSCOCO", batch_size_dict["MSCOCO"], "torch_compile")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Vizwiz", batch_size_dict["Vizwiz"], "torch_compile")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Coco_Image", batch_size_dict["Coco_Image"], "torch_compile")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Coco_Image_small", batch_size_dict["Coco_Image_small"], "torch_compile")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("HumanEval", batch_size_dict["HumanEval"], "torch_compile")).values()]),
-#     # sum([np.average(v[5:]) for v in get_latency(get_folder("HumanEval_small", 4, "torch_compile")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("S2ST", batch_size_dict["S2ST"], "torch_compile")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("S2TT", batch_size_dict["S2TT"], "torch_compile")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("T2ST", batch_size_dict["T2ST"], "torch_compile")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("T2TT", batch_size_dict["T2TT"], "torch_compile")).values()]),
-# ]
-
-
-# torch_compile_autoquant = [
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("MSCOCO", batch_size_dict["MSCOCO"], "torch_compile_autoquant")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Vizwiz", batch_size_dict["Vizwiz"], "torch_compile_autoquant")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Coco_Image", batch_size_dict["Coco_Image"], "torch_compile_autoquant")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("Coco_Image_small", batch_size_dict["Coco_Image_small"], "torch_compile_autoquant")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("HumanEval", batch_size_dict["HumanEval"], "torch_compile_autoquant")).values()]),
-#     # sum([np.average(v[5:]) for v in get_latency(get_folder("HumanEval_small", 4, "torch_compile_autoquant")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("S2ST", batch_size_dict["S2ST"], "torch_compile_autoquant")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("S2TT", batch_size_dict["S2TT"], "torch_compile_autoquant")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("T2ST", batch_size_dict["T2ST"], "torch_compile_autoquant")).values()]),
-#     sum([np.average(v[5:]) for v in get_latency(get_folder("T2TT", batch_size_dict["T2TT"], "torch_compile_autoquant")).values()]),
-# ]
-  
-# create data 
-x = np.arange(len(baseline)) 
-width = 0.2
-  
-# plot data in grouped manner of bar type 
-plt.bar(x-0.2, baseline, width, color='cyan') 
-plt.bar(x, torch_compile, width, color='orange') 
-plt.bar(x+0.2, torch_compile_autoquant, width, color='green') 
-
-plt.xticks(x, ['MSCOCO', 
-                'Vizwiz',
-                'Coco_Image',
-                'Coco_Image_small',
-               'HumanEval', 
-            #    'HumanEval_small', 
-               'S2ST', 
-               'S2TT', 
-               'T2ST', 
-               'T2TT'], rotation=90) 
-plt.xlabel("Teams") 
-plt.ylabel("Average End-to-end Inference Tie (ms)") 
-plt.legend(["Baseline", "Torch.compile", "Torch.compile+Autoquant"])
-plt.grid(lw=0.2)
-plt.show() 
-
+    plot(batch_size_dict)
 # %%
