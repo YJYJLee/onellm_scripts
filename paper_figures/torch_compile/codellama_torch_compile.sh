9a6132009e285fe4e72f4cb0d55e25c63cc6b232
YJYJLee <jane001356@gmail.com>
2024-06-25 03:43:18 +0000

    update

--- paper_figures/torch_compile/codellama_torch_compile.sh
+++ paper_figures/torch_compile/codellama_torch_compile.sh
@@ -5,3 +5,11 @@ TORCH_COMPILE=True BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta
 #7B
 TORCH_COMPILE=True BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta-llama/CodeLlama-7b-hf --max_length_generation 10000 --tasks humaneval --temperature 0.2 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
 TORCH_COMPILE=True BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta-llama/CodeLlama-7b-hf --max_length_generation 10000 --tasks mbpp --temperature 0.1 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
+
+## Baseline
+#34B
+TORCHINDUCTOR_CACHE_DIR=/fsx-atom/shared/yj_cache TRITON_CACHE_DIR=/fsx-atom/shared/yj_cache BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta-llama/CodeLlama-34b-hf --max_length_generation 10000 --tasks humaneval --temperature 0.2 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
+TORCHINDUCTOR_CACHE_DIR=/fsx-atom/shared/yj_cache TRITON_CACHE_DIR=/fsx-atom/shared/yj_cache BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta-llama/CodeLlama-34b-hf --max_length_generation 10000 --tasks mbpp --temperature 0.1 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
+#7B
+TORCHINDUCTOR_CACHE_DIR=/fsx-atom/shared/yj_cache TRITON_CACHE_DIR=/fsx-atom/shared/yj_cache BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta-llama/CodeLlama-7b-hf --max_length_generation 10000 --tasks humaneval --temperature 0.2 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
+TORCHINDUCTOR_CACHE_DIR=/fsx-atom/shared/yj_cache TRITON_CACHE_DIR=/fsx-atom/shared/yj_cache BATCH_SIZE=$BATCH_SIZE accelerate launch main.py --model meta-llama/CodeLlama-7b-hf --max_length_generation 10000 --tasks mbpp --temperature 0.1 --n_samples 1 --batch_size $BATCH_SIZE --allow_code_execution --precision fp16
